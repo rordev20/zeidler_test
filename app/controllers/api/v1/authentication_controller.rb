7e8367fa0b3a8884bed289acb9b41ad5fe5927ed
Rohit Patel <rohit.patel@infibeam.net>
2020-03-07 14:18:52 +0530

    api to load user after authentication

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,17 +1,20 @@
 class Api::V1::AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
 
-  # POST /auth/login
+  # POST api/v1/auth/login
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { data: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     email: @user.email }, message: I18n.t('global.success'), status: 200}
+      render json: { data: {token: token} , message: I18n.t('global.success'), status: 200}
     else
       render json: { error: 'unauthorized', message: I18n.t('global.failure'), status: 401}
     end
+  end
+
+  def user
+    render json: { data: { user: @current_user }, message: I18n.t('global.success'), status: 200}
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, param: :_username
       post '/auth/login', to: 'authentication#login'
+      get '/auth/user', to: 'authentication#user'
       get '/*a', to: 'application#not_found'
     end
   end
